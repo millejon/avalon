61a8ea8bec004de986481c2b0adfd3a55edefc21
Jonathan Miller <millejon@oregonstate.edu>
2023-12-24 18:13:22 -0800

    Add adding songs to playlist

--- avalon/library.py
+++ avalon/library.py
@@ -18,7 +18,7 @@ def view_all_artists():
     return render_template("all_artists.html", artists=artists)
 
 
-@bp.route("/artists/<int:id>", methods=("GET",))
+@bp.route("/artists/<int:id>/", methods=("GET",))
 def view_artist(id: int):
     return render_template("artist.html", artist=Artist(id))
 
@@ -33,7 +33,7 @@ def view_all_albums():
     return render_template("all_albums.html", albums=albums)
 
 
-@bp.route("/albums/<int:id>", methods=("GET",))
+@bp.route("/albums/<int:id>/", methods=("GET",))
 def view_album(id: int):
     return render_template("album.html", album=Album(id))
 
@@ -43,6 +43,16 @@ def create_playlist():
     db.execute_write_query(
         query=database["playlists"]["queries"]["write"],
         data=(request.form["playlist_name"],),
+    )
+
+    return redirect(request.referrer)
+
+
+@bp.route("/playlists/<int:playlist_id>/songs/<int:song_id>/", methods=("POST",))
+def add_song_to_playlist(playlist_id, song_id):
+    db.execute_write_query(
+        query=database["playlists_songs"]["queries"]["write"],
+        data=(playlist_id, song_id),
     )
 
     return redirect(request.referrer)
