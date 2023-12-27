b0a02487163e6f4fe407acddcd63688a391580c5
Jonathan Miller <millejon@oregonstate.edu>
2023-12-26 21:45:08 -0800

    Add routes for viewing all songs/albums of artists/hubs

--- avalon/library.py
+++ avalon/library.py
@@ -10,8 +10,10 @@ from avalon.data import database
 bp = Blueprint("library", __name__, url_prefix="/")
 
 
-def render_page(template: str, content):
-    return render_template(template, content=content, playlists=get_playlists())
+def render_page(template: str, content, key: str = None):
+    return render_template(
+        template, content=content, key=key, playlists=get_playlists()
+    )
 
 
 @bp.route("/", methods=("GET",))
@@ -26,12 +28,29 @@ def view_all_artists():
     )
     artists = [Artist(data["id"]) for data in artists_data]
 
-    return render_page(template="all_artists.html", content=artists)
+    return render_page(template="all_artists.html", content=artists, key="artists")
 
 
 @bp.route("/artists/<int:id>", methods=("GET",))
 def view_artist(id: int):
-    return render_page(template="artist.html", content=Artist(id))
+    return render_page(template="artist.html", content=Artist(id), key="artists")
+
+
+@bp.route("/artists/<int:id>/songs/", methods=("GET",))
+def view_artist_songs(id: int):
+    return render_page(template="artist_songs.html", content=Artist(id))
+
+
+@bp.route("/artists/<int:id>/produced/", methods=("GET",))
+def view_artist_produced_songs(id: int):
+    return render_page(template="artist_produced_songs.html", content=Artist(id))
+
+
+@bp.route("/artists/<int:id>/<album_type>/", methods=("GET",))
+def view_artist_albums(id: int, album_type: str):
+    return render_page(
+        template="artist_albums.html", content=Artist(id), key=album_type
+    )
 
 
 @bp.route("/albums/", methods=("GET",))
@@ -93,9 +112,19 @@ def view_all_hubs():
     )
     hubs = [Hub(data["id"]) for data in hubs_data]
 
-    return render_page(template="all_hubs.html", content=hubs)
+    return render_page(template="all_artists.html", content=hubs, key="hubs")
 
 
 @bp.route("/hubs/<int:id>", methods=("GET",))
 def view_hub(id: int):
-    return render_page(template="hub.html", content=Hub(id))
+    return render_page(template="artist.html", content=Hub(id), key="hubs")
+
+
+@bp.route("/hubs/<int:id>/songs/", methods=("GET",))
+def view_hub_songs(id: int):
+    return render_page(template="artist_songs.html", content=Hub(id))
+
+
+@bp.route("/hubs/<int:id>/albums/", methods=("GET",))
+def view_hub_albums(id: int):
+    return render_page(template="artist_albums.html", content=Hub(id), key="albums")
