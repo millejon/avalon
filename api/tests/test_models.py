3a00dc4e4424d047645f65505eef2a4a3a4658da
Jonathan <millejon327@gmail.com>
2024-06-16 12:39:47 -0700

    Update tests for Artist model
    Change how test data is set up before tests are run.

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -7,23 +7,18 @@ from api import models
 class ArtistModelTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        models.Artist.objects.create(name="2pac")
+        cls.artist = models.Artist.objects.create(name="2pac")
 
     def test_artist_creation(self):
-        artist = models.Artist.objects.get(id=1)
-
-        self.assertEqual(artist.name, "2pac")
+        self.assertEqual(self.artist.name, "2pac")
 
     def test_name_max_length(self):
-        artist = models.Artist.objects.get(id=1)
-        max_length = artist._meta.get_field("name").max_length
+        max_length = self.artist._meta.get_field("name").max_length
 
         self.assertEqual(max_length, 100)
 
     def test_artist_str_method(self):
-        artist = models.Artist.objects.get(id=1)
-
-        self.assertEqual(str(artist), "2pac")
+        self.assertEqual(str(self.artist), "2pac")
 
     def test_artist_get_absolute_url(self):
         # TODO: Add test after coding front-end views
