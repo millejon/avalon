8fb1836df8cc739383d8706c04e4ea6fbeed49be
Jonathan Miller <millejon327@gmail.com>
2024-07-04 16:39:03 -0700

    Add Django Ninja framework

--- avalon/settings.py
+++ avalon/settings.py
@@ -15,6 +15,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "catalog.apps.CatalogConfig",
+    "ninja",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
