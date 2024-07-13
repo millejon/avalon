550bea5916cbab38768ae8ddffe181e7f3763dd9
Jonathan <millejon327@gmail.com>
2024-07-13 13:08:32 -0700

    Refactor artist tests

--- api/tests/test_artists.py
+++ api/tests/test_artists.py
@@ -3,12 +3,12 @@ from django.db import IntegrityError
 from django.urls import reverse
 
 
-class CreateArtistTestCase(TestCase):
+class CreateArtist(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
 
-    def test_create_artist(self):
+    def test_create_valid_artist_status_code(self):
         response = self.client.post(
             reverse("api-1.0:create_artist"),
             {"name": "Future"},
@@ -17,7 +17,12 @@ class CreateArtistTestCase(TestCase):
 
         self.assertEqual(response.status_code, 201)
 
-        response = response.json()
+    def test_create_valid_artist_json_response(self):
+        response = self.client.post(
+            reverse("api-1.0:create_artist"),
+            {"name": "Future"},
+            content_type="application/json",
+        ).json()
         artist_id = response["id"]
 
         self.assertEqual(response["name"], "Future")
@@ -65,7 +70,7 @@ class CreateArtistTestCase(TestCase):
     def test_create_artist_with_missing_required_fields(self):
         response = self.client.post(
             reverse("api-1.0:create_artist"),
-            {"label": "Def Jam Recordings"},
+            {"label": "Freebandz"},
             content_type="application/json",
         )
 
@@ -74,42 +79,44 @@ class CreateArtistTestCase(TestCase):
     def test_create_artist_with_invalid_values(self):
         response = self.client.post(
             reverse("api-1.0:create_artist"),
-            {"name": 42},
+            {"name": 2003},
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, 422)
 
 
-class RetrieveArtistTestCase(TestCase):
+class RetrieveArtist(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
-        cls.artist = cls.client.post(
+        cls.tinashe = cls.client.post(
             reverse("api-1.0:create_artist"),
             {"name": "Tinashe"},
             content_type="application/json",
         ).json()
 
-    def test_retrieve_artist(self):
-        artist_id = self.artist["id"]
+    def test_retrieve_artist_status_code(self):
         response = self.client.get(
-            reverse("api-1.0:retrieve_artist", kwargs={"id": artist_id})
+            reverse("api-1.0:retrieve_artist", kwargs={"id": self.tinashe["id"]})
         )
 
         self.assertEqual(response.status_code, 200)
 
-        response = response.json()
+    def test_retrieve_artist_json_response(self):
+        response = self.client.get(
+            reverse("api-1.0:retrieve_artist", kwargs={"id": self.tinashe["id"]})
+        ).json()
 
         self.assertEqual(response["name"], "Tinashe")
-        self.assertTrue(response["url"].endswith(f"/api/v1/artists/{artist_id}"))
+        self.assertTrue(response["url"].endswith(f"/api/v1/artists/{self.tinashe["id"]}"))
         self.assertIsNone(response["albums"])
         self.assertIsNone(response["singles"])
         self.assertIsNone(response["songs"])
         self.assertIsNone(response["songs_produced"])
 
     def test_retrieve_unknown_artist(self):
-        artist_id = self.artist["id"] + 1
+        artist_id = self.tinashe["id"] + 1
         response = self.client.get(
             reverse("api-1.0:retrieve_artist", kwargs={"id": artist_id})
         )
@@ -120,31 +127,34 @@ class RetrieveArtistTestCase(TestCase):
         )
 
 
-class UpdateArtistTestCase(TestCase):
+class UpdateArtist(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
-        cls.artist = cls.client.post(
+        cls.don_toliver = cls.client.post(
             reverse("api-1.0:create_artist"),
-            {"name": "Chinx"},
+            {"name": "Don Toliver"},
             content_type="application/json",
         ).json()
 
-    def test_update_artist(self):
+    def test_update_artist_status_code(self):
         response = self.client.put(
-            reverse("api-1.0:update_artist", kwargs={"id": self.artist["id"]}),
-            {"name": "Chinx Drugz"},
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"name": "Donny Womack"},
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, 200)
 
-        response = response.json()
-        artist_id = response["id"]
+    def test_update_artist_json_response(self):
+        response = self.client.put(
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"name": "Donny Womack"},
+            content_type="application/json",
+        ).json()
 
-        self.assertEqual(self.artist["id"], artist_id)
-        self.assertEqual(response["name"], "Chinx Drugz")
-        self.assertTrue(response["url"].endswith(f"/api/v1/artists/{artist_id}"))
+        self.assertEqual(response["name"], "Donny Womack")
+        self.assertTrue(response["url"].endswith(f"/api/v1/artists/{self.don_toliver["id"]}"))
         self.assertIsNone(response["albums"])
         self.assertIsNone(response["singles"])
         self.assertIsNone(response["songs"])
@@ -152,29 +162,29 @@ class UpdateArtistTestCase(TestCase):
 
     def test_update_artist_with_extraneous_whitespace(self):
         response = self.client.put(
-            reverse("api-1.0:update_artist", kwargs={"id": self.artist["id"]}),
-            {"name": "   Chinx Drugz   "},
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"name": "   Donny Womack   "},
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["name"], "Chinx Drugz")
+        self.assertEqual(response.json()["name"], "Donny Womack")
 
     def test_update_artist_with_extraneous_fields(self):
         response = self.client.put(
-            reverse("api-1.0:update_artist", kwargs={"id": self.artist["id"]}),
-            {"name": "Chinx Drugz", "alias": "Chinx"},
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"name": "Donny Womack", "alias": "Don Toliver"},
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()["name"], "Chinx Drugz")
+        self.assertEqual(response.json()["name"], "Donny Womack")
         self.assertFalse("alias" in response.json().keys())
 
     def test_update_artist_with_missing_required_fields(self):
         response = self.client.put(
-            reverse("api-1.0:update_artist", kwargs={"id": self.artist["id"]}),
-            {"label": "Def Jam Recordings"},
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"label": "Cactus Jack Records"},
             content_type="application/json",
         )
 
@@ -182,15 +192,15 @@ class UpdateArtistTestCase(TestCase):
 
     def test_update_artist_with_invalid_values(self):
         response = self.client.put(
-            reverse("api-1.0:update_artist", kwargs={"id": self.artist["id"]}),
-            {"name": 42},
+            reverse("api-1.0:update_artist", kwargs={"id": self.don_toliver["id"]}),
+            {"name": 2015},
             content_type="application/json",
         )
 
         self.assertEqual(response.status_code, 422)
 
     def test_update_unknown_artist(self):
-        artist_id = self.artist["id"] + 1
+        artist_id = self.don_toliver["id"] + 1
         response = self.client.put(
             reverse("api-1.0:update_artist", kwargs={"id": artist_id}),
             {"name": "Chinx Drugz"},
@@ -203,36 +213,45 @@ class UpdateArtistTestCase(TestCase):
         )
 
 
-class DeleteArtistTestCase(TestCase):
+class DeleteArtist(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.client = Client()
-        cls.artist = cls.client.post(
+        cls.french_montana = cls.client.post(
             reverse("api-1.0:create_artist"),
             {"name": "French Montana"},
             content_type="application/json",
         ).json()
 
-    def test_delete_artist(self):
-        artist_id = self.artist["id"]
-        response = self.client.delete(
-            reverse("api-1.0:delete_artist", kwargs={"id": artist_id})
+    def test_delete_artist_successful(self):
+        self.client.delete(
+            reverse("api-1.0:delete_artist", kwargs={"id": self.french_montana["id"]})
         )
-
-        self.assertEqual(response.status_code, 204)
-        self.assertEqual(response.content, b"")
-
         response = self.client.get(
-            reverse("api-1.0:retrieve_artist", kwargs={"id": artist_id})
+            reverse("api-1.0:retrieve_artist", kwargs={"id": self.french_montana["id"]})
         )
 
         self.assertEqual(response.status_code, 404)
         self.assertEqual(
-            response.json()["error"], f"Artist with id = {artist_id} does not exist."
+            response.json()["error"], f"Artist with id = {self.french_montana["id"]} does not exist."
         )
 
+    def test_delete_artist_status_code(self):
+        response = self.client.delete(
+            reverse("api-1.0:delete_artist", kwargs={"id": self.french_montana["id"]})
+        )
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_artist_json_response(self):
+        response = self.client.delete(
+            reverse("api-1.0:delete_artist", kwargs={"id": self.french_montana["id"]})
+        )
+
+        self.assertEqual(response.content, b"")
+
     def test_delete_unknown_artist(self):
-        artist_id = self.artist["id"] + 1
+        artist_id = self.french_montana["id"] + 1
         response = self.client.delete(
             reverse("api-1.0:delete_artist", kwargs={"id": artist_id})
         )
