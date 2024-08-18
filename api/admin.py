474a703cee47cefa54713dfbd985999f54011072
Jonathan Miller <millejon327@gmail.com>
2024-08-17 17:21:30 -0700

    Add AlbumArtist model to Django admin

--- api/admin.py
+++ api/admin.py
@@ -4,6 +4,7 @@ from api import models
 
 admin.site.register(models.Artist)
 admin.site.register(models.Album)
+admin.site.register(models.AlbumArtist)
 admin.site.register(models.Disc)
 admin.site.register(models.Song)
 admin.site.register(models.SongArtist)
