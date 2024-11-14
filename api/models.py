c49f0b428373c51b852f1a5943629db84cd99f26
Jonathan Miller <millejon327@gmail.com>
2024-11-13 18:06:47 -0800

    Add Artist database model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,18 @@
-# from django.db import models
+from django.db import models
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    hometown = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        constraints = [
+            models.UniqueConstraint(
+                models.functions.Lower("name"),
+                name="duplicate_artist_case_insensitive_match",
+            ),
+        ]
