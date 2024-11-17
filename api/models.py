6f8b5648e6c99b87b3c54f67ddf35595cf421f66
Jonathan Miller <millejon327@gmail.com>
2024-11-16 18:57:11 -0800

    Add Album and AlbumArtist database models

--- api/models.py
+++ api/models.py
@@ -21,3 +21,44 @@ class Artist(models.Model):
                 name="duplicate_artist_case_insensitive_match",
             ),
         ]
+
+
+class Album(models.Model):
+    ALBUM_TYPES = {"album": "album", "multidisc": "multidisc", "single": "single"}
+
+    title = models.CharField(max_length=600)
+    artists = models.ManyToManyField(
+        Artist, through="AlbumArtist", related_name="album_artists"
+    )
+    release_date = models.DateField()
+    label = models.CharField(max_length=100, blank=True)
+    album_type = models.CharField(max_length=10, choices=ALBUM_TYPES, default="album")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["artists__name", "release_date"]
+        constraints = [
+            models.UniqueConstraint(
+                models.functions.Lower("title"),
+                "release_date",
+                name="duplicate_album",
+            ),
+        ]
+
+
+class AlbumArtist(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.artist.name} - {self.album.title}"
+
+    class Meta:
+        ordering = ["id"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["album", "artist"], name="duplicate_album_artist"
+            )
+        ]
