052a3d5c5c9be9e3058d4624337efbac04158abc
Jonathan Miller <millejon327@gmail.com>
2024-11-25 17:24:20 -0800

    Add utility to retrieve Artist objects from database

--- api/utilities.py
+++ api/utilities.py
@@ -1,5 +1,47 @@
 from typing import Any
 
+from api import models
+
+
+def get_artists(artist_data: list[dict[str, str]]) -> list[models.Artist]:
+    """Retrieve Artist objects from the database.
+
+    This utility accepts a list of dictionaries containing artist
+    metadata. For each artist in the list, the corresponding Artist
+    object will be retrieved from the database. If the artist does not
+    yet exist in the database, then a new Artist object will be created.
+    If the artist metadata has a value for the hometown attribute while
+    the corresponding Artist object does not, then the Artist object
+    will be updated accordingly.
+
+    Arguments:
+        artist_data (list) -- A list of dictionaries where each
+        dictionary contains the metadata for one artist. Each dictionary
+        should have the following fields:
+            name (str) - The name of the artist or group (e.g. "Jay-Z")
+            [required]
+            hometown (str) - The city the artist is most associated with
+            (e.g. "New York, NY") [optional]
+
+    Returns:
+        artists (list) -- A list of Artist objects.
+    """
+    artists = []
+
+    for data in artist_data:
+        data = strip_whitespace(data)
+        try:
+            artist = models.Artist.objects.get(name__iexact=data["name"])
+        except models.Artist.DoesNotExist:
+            artist = models.Artist.objects.create(**data)
+        finally:
+            if data["hometown"] and artist.hometown != data["hometown"]:
+                artist.hometown = data["hometown"]
+                artist.save()
+            artists.append(artist)
+
+    return artists
+
 
 def strip_whitespace(data: dict[Any, Any]) -> dict[Any, Any]:
     """Remove extraneous whitespace from string values in a dictionary.
