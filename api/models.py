117be11d6033ba54f18d2b0f0b3a749bba6fc39d
Jonathan Miller <millejon327@gmail.com>
2024-11-29 15:00:36 -0800

    Add Song, SongArtist, and SongProducer database models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core import validators
 from django.urls import reverse
 
 
@@ -76,5 +77,82 @@ class AlbumArtist(models.Model):
         constraints = [
             models.UniqueConstraint(
                 fields=["album", "artist"], name="duplicate_album_artist"
+            )
+        ]
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=600)
+    artists = models.ManyToManyField(
+        Artist, through="SongArtist", related_name="song_artists"
+    )
+    producers = models.ManyToManyField(
+        Artist, through="SongProducer", related_name="song_producers"
+    )
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    disc = models.PositiveSmallIntegerField(
+        default=1, validators=[validators.MinValueValidator(1)]
+    )
+    track_number = models.PositiveSmallIntegerField(
+        validators=[validators.MinValueValidator(1)]
+    )
+    length = models.PositiveSmallIntegerField()
+    path = models.CharField(max_length=1000, unique=True)
+    play_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.track_number}. {self.title} [{self.album.title}]"
+
+    class Meta:
+        ordering = ["-play_count", "-album__release_date", "disc", "track_number"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["album", "disc", "track_number"], name="duplicate_track_number"
+            ),
+            models.UniqueConstraint(
+                models.functions.Lower("path"),
+                name="duplicate_song_case_insensitive_match",
+            ),
+            models.CheckConstraint(
+                condition=models.Q(disc__gte=1),
+                name="disc_number_must_be_greater_than_0",
+            ),
+            models.CheckConstraint(
+                condition=models.Q(track_number__gte=1),
+                name="track_number_must_be_greater_than_0",
+            ),
+        ]
+
+
+class SongArtist(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    group = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return f"{self.artist.name} - {self.song.title}"
+
+    class Meta:
+        ordering = ["id"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["song", "artist"], name="duplicate_song_artist"
+            )
+        ]
+
+
+class SongProducer(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    producer = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.producer.name} - {self.song.title} [{self.role}]"
+
+    class Meta:
+        ordering = ["id"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["song", "producer"], name="duplicate_producer"
             )
         ]
