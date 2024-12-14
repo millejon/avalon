98e78705c298324e5978b10a20c5e297f74bed1e
Jonathan Miller <millejon327@gmail.com>
2024-12-14 14:50:46 -0800

    Refactor tests for create album endpoint

--- api/tests/test_albums.py
+++ api/tests/test_albums.py
@@ -10,195 +10,134 @@ class CreateAlbumTestCase(TestCase):
     def setUpTestData(cls):
         cls.client = Client()
 
-    def test_create_valid_album_status_code(self):
-        response = self.client.post(
+    def setUp(self):
+        self.metadata = {
+            "title": "Vol. 2, Hard Knock Life",
+            "artists": [
+                {
+                    "name": "Jay-Z",
+                    "hometown": "New York, NY",
+                }
+            ],
+            "release_date": "1998-09-29",
+            "label": "Roc-A-Fella Records",
+            "album_type": "album",
+        }
+
+    def send_post_request(self, data: dict[str, Any]) -> HttpResponse:
+        """Send POST request to API endpoint that creates an Album object.
+
+        Arguments:
+            data (dict) -- A dictionary that contains the metadata for
+            the album to be added to the database.
+
+        Returns:
+            HttpResponse object with the results of the POST request.
+        """
+        return self.client.post(
             reverse("api:create_album"),
-            data={
-                "title": "Vol. 2, Hard Knock Life",
-                "artists": [
-                    {
-                        "name": "Jay-Z",
-                        "hometown": "New York, NY",
-                    }
-                ],
-                "release_date": "1998-09-29",
-                "label": "Roc-A-Fella Records",
-                "album_type": "album",
-            },
+            data=data,
             content_type="application/json",
         )
+
+    def test_create_album_status_code(self):
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 201)
 
-    def test_create_valid_album_json_response(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "Vol. 2, Hard Knock Life",
-                "artists": [
-                    {
-                        "name": "Jay-Z",
-                        "hometown": "New York, NY",
-                    }
-                ],
-                "release_date": "1998-09-29",
-                "label": "Roc-A-Fella Records",
-                "album_type": "album",
-            },
-            content_type="application/json",
-        ).json()
+    def test_create_album_json_response_title(self):
+        response = self.send_post_request(self.metadata).json()
 
-        self.assertEqual(response["title"], "Vol. 2, Hard Knock Life")
-        self.assertEqual(response["artists"][0]["name"], "Jay-Z")
-        self.assertTrue(
-            response["artists"][0]["url"].endswith(
-                f"/api/artists/{response["artists"][0]["id"]}"
-            )
-        )
-        self.assertEqual(response["release_date"], "1998-09-29")
-        self.assertEqual(response["label"], "Roc-A-Fella Records")
-        self.assertEqual(response["album_type"], "album")
+        self.assertEqual(response["title"], self.metadata["title"])
+
+    def test_create_album_json_response_artists(self):
+        response = self.send_post_request(self.metadata).json()
+        artists = response["artists"]
+
+        self.assertEqual(artists[0]["name"], self.metadata["artists"][0]["name"])
+        self.assertTrue(artists[0]["url"].endswith(f"/api/artists/{artists[0]["id"]}"))
+        self.assertFalse("hometown" in artists[0].keys())
+
+    def test_create_album_json_response_release_date(self):
+        response = self.send_post_request(self.metadata).json()
+
+        self.assertEqual(response["release_date"], self.metadata["release_date"])
+
+    def test_create_album_json_response_label(self):
+        response = self.send_post_request(self.metadata).json()
+
+        self.assertEqual(response["label"], self.metadata["label"])
+
+    def test_create_album_json_response_tracklist(self):
+        response = self.send_post_request(self.metadata).json()
+        tracklist = response["tracklist"]
+
+        self.assertEqual(tracklist["count"], 0)
+        self.assertTrue(tracklist["url"].endswith(f"api/albums/{response["id"]}/songs"))
+
+    def test_create_album_json_response_length(self):
+        response = self.send_post_request(self.metadata).json()
+
+        self.assertEqual(response["length"], 0)
+
+    def test_create_album_json_response_album_type(self):
+        response = self.send_post_request(self.metadata).json()
+
+        self.assertEqual(response["album_type"], self.metadata["album_type"])
+
+    def test_create_album_json_response_url(self):
+        response = self.send_post_request(self.metadata).json()
+
         self.assertTrue(response["url"].endswith(f"api/albums/{response["id"]}"))
 
-    def test_create_valid_album_without_optional_fields_status_code(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "Coming Of Age",
-                "artists": [
-                    {
-                        "name": "Memphis Bleek",
-                        "hometown": "New York, NY",
-                    }
-                ],
-                "release_date": "1999-08-03",
-            },
-            content_type="application/json",
-        )
+    def test_create_album_without_label(self):
+        del self.metadata["label"]
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 201)
+        self.assertIsNone(response.json()["label"])
 
-    def test_create_valid_album_without_optional_fields_json_response(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "Coming Of Age",
-                "artists": [
-                    {
-                        "name": "Memphis Bleek",
-                        "hometown": "New York, NY",
-                    }
-                ],
-                "release_date": "1999-08-03",
-            },
-            content_type="application/json",
-        ).json()
+    def test_create_album_without_album_type(self):
+        del self.metadata["album_type"]
+        response = self.send_post_request(self.metadata)
 
-        self.assertEqual(response["title"], "Coming Of Age")
-        self.assertEqual(response["artists"][0]["name"], "Memphis Bleek")
-        self.assertTrue(
-            response["artists"][0]["url"].endswith(
-                f"/api/artists/{response["artists"][0]["id"]}"
-            )
-        )
-        self.assertEqual(response["release_date"], "1999-08-03")
-        self.assertIsNone(response["label"])
-        self.assertEqual(response["album_type"], "album")
-        self.assertTrue(response["url"].endswith(f"api/albums/{response["id"]}"))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()["album_type"], "album")
 
     def test_create_album_with_extraneous_whitespace(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "  The    Professional ",
-                "artists": [
-                    {
-                        "name": " DJ   Clue  ",
-                        "hometown": "New York, NY",
-                    }
-                ],
-                "release_date": "1998-12-15",
-                "label": "   Roc-A-Fella  Records   ",
-                "album_type": "album",
-            },
-            content_type="application/json",
-        )
+        self.metadata["title"] = "   Vol. 2,  Hard    Knock Life  "
+        self.metadata["artists"][0]["name"] = "  Jay-Z "
+        self.metadata["label"] = " Roc-A-Fella   Records   "
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()["title"], "The Professional")
-        self.assertEqual(response.json()["artists"][0]["name"], "DJ Clue")
+        self.assertEqual(response.json()["title"], "Vol. 2, Hard Knock Life")
+        self.assertEqual(response.json()["artists"][0]["name"], "Jay-Z")
         self.assertEqual(response.json()["label"], "Roc-A-Fella Records")
 
     def test_create_album_with_extraneous_fields(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "The Truth",
-                "artists": [
-                    {
-                        "name": "Beanie Sigel",
-                        "hometown": "Philadelphia, PA",
-                    }
-                ],
-                "release_date": "2000-02-29",
-                "label": "Roc-A-Fella Records",
-                "album_type": "album",
-                "executive_producers": ["Jay-Z", "Damon Dash", "Kareem Burke"],
-            },
-            content_type="application/json",
-        )
+        self.metadata["art_design"] = "The Drawing Board"
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.json()["title"], "The Truth")
-        self.assertFalse("executive_producers" in response.json().keys())
+        self.assertFalse("art_design" in response.json().keys())
 
     def test_create_duplicate_album(self):
-        self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "Reasonable Doubt",
-                "artists": [{"name": "Jay-Z", "hometown": "New York, NY"}],
-                "release_date": "1996-06-25",
-                "label": "Roc-A-Fella Records",
-                "album_type": "album",
-            },
-            content_type="application/json",
-        )
+        self.send_post_request(self.metadata)
+        response = self.send_post_request(self.metadata)
 
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": "reasonable doubt",
-                "artists": [{"name": "Jay-Z"}],
-                "release_date": "1996-06-25",
-            },
-            content_type="application/json",
-        )
-
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 409)
         self.assertEqual(response.json()["error"], "Album already exists in database.")
 
     def test_create_album_with_missing_required_fields(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            {"title": "All Money Is Legal"},
-            content_type="application/json",
-        )
+        del self.metadata["release_date"]
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 422)
 
     def test_create_album_with_invalid_values(self):
-        response = self.client.post(
-            reverse("api:create_album"),
-            data={
-                "title": 534,
-                "artists": [{"name": "Memphis Bleek", "hometown": "New York, NY"}],
-                "release_date": "2005-05-17",
-                "label": "Roc-A-Fella Records",
-                "album_type": "album",
-            },
-            content_type="application/json",
-        )
+        self.metadata["release_date"] = 19980929
+        response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 422)
 
