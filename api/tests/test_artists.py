83182c14eef0351c264f1ca0144d270ab23aaa5a
Jonathan Miller <millejon327@gmail.com>
2024-12-16 22:43:14 -0800

    Add tests for retrieve artist endpoint

--- api/tests/test_artists.py
+++ api/tests/test_artists.py
@@ -124,3 +124,97 @@ class CreateArtistTestCase(TestCase):
         response = self.send_post_request(self.metadata)
 
         self.assertEqual(response.status_code, 422)
+
+
+class RetrieveArtistTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+
+    def setUp(self):
+        self.black_rob = self.client.post(
+            reverse("api:create_artist"),
+            data={"name": "Black Rob", "hometown": "New York, NY"},
+            content_type="application/json",
+        ).json()
+
+    def send_get_request(self, id: str) -> HttpResponse:
+        """Send GET request to API endpoint that retrieves an Artist object.
+
+        Arguments:
+            id (str) -- The primary key of the artist to retrieve.
+
+        Returns:
+            HttpResponse object with the results of the GET request.
+        """
+        return self.client.get(reverse("api:retrieve_artist", kwargs={"id": id}))
+
+    def test_retrieve_artist_status_code(self):
+        response = self.send_get_request(self.black_rob["id"])
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_artist_json_response_name(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+
+        self.assertEqual(response["name"], self.black_rob["name"])
+
+    def test_retrieve_artist_json_response_hometown(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+
+        self.assertEqual(response["hometown"], self.black_rob["hometown"])
+
+    def test_retrieve_artist_json_response_albums(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+        albums = response["albums"]
+
+        self.assertEqual(albums["count"], 0)
+        self.assertTrue(
+            albums["url"].endswith(f"/api/artists/{self.black_rob["id"]}/albums")
+        )
+
+    def test_retrieve_artist_json_response_singles(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+        singles = response["singles"]
+
+        self.assertEqual(singles["count"], 0)
+        self.assertTrue(
+            singles["url"].endswith(f"/api/artists/{self.black_rob["id"]}/singles")
+        )
+
+    def test_retrieve_artist_json_response_songs(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+        songs = response["songs"]
+
+        self.assertEqual(songs["count"], 0)
+        self.assertTrue(
+            songs["url"].endswith(f"/api/artists/{self.black_rob["id"]}/songs")
+        )
+
+    def test_retrieve_artist_json_response_songs_produced(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+        songs_produced = response["songs_produced"]
+
+        self.assertEqual(songs_produced["count"], 0)
+        self.assertTrue(
+            songs_produced["url"].endswith(
+                f"/api/artists/{self.black_rob["id"]}/songs-produced"
+            )
+        )
+
+    def test_retrieve_artist_json_response_url(self):
+        response = self.send_get_request(self.black_rob["id"]).json()
+
+        self.assertTrue(
+            response["url"].endswith(f"/api/artists/{self.black_rob["id"]}")
+        )
+
+    def test_retrieve_artist_that_does_not_exist(self):
+        unknown_artist = str(int(self.black_rob["id"]) + 100)
+        response = self.send_get_request(unknown_artist)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json()["error"],
+            f"Artist with id = {unknown_artist} does not exist.",
+        )
